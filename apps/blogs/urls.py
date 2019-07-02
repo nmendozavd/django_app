70c180ce23f9011e7eeea3bb4acb54372644d1b0
Noel Mendoza <nmendozavd@gmail.com>
2019-07-02 01:01:21 -0700

    first commit

---
+++ apps/blogs/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views           # This line is new!
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^new/', views.new),
+    url(r'^create/', views.create),
+    url(r'^(?P<number>[0-9]+)/$', views.show),
+    url(r'^(?P<number>[0-9]+)/edit/', views.edit),
+    url(r'^(?P<number>[0-9]+)/delete/', views.destroy)
+    
+]
+
+
